c422214f80f25458eefbd0dfb43478a8ff519934
Akshay-770 <gogarkanabhai@gmail.com>
2025-07-04 11:17:41 +0530

    Akkkkkk

--- Program.cs
+++ Program.cs
@@ -11,6 +11,15 @@ namespace net_bd
         static void Main(string[] args)
         {
 
+            Console.WriteLine("inter a");
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inter b");
+            int b=Convert.ToInt32(Console.ReadLine());
+            int sum;
+            sum = a + b;
+            // string interploation
+            Console.WriteLine($"sum is {a} + {b} "+sum);
+            Console.ReadLine();
         }
     }
 }
